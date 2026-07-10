b528c920ccf5ffb2cfed130e56ce24c58aa66700
Joseph <177612090+josephkwok001@users.noreply.github.com>
2026-07-10 17:30:29 +0800

    Nothing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const path = require("path")
 const express = require("express")
 const hbs = require("hbs")
+const geocode = require("./utils/geocode")
+const forecast = require("./utils/forecast")
+require("dotenv").config({ path: path.join(__dirname, "../.env") })
 
 const app = express() 
 
@@ -47,11 +50,25 @@ app.get("/weather", (req, res) => {
             error: "You must provide an address!"
         })
     }
-    res.send({
-        forecast: "It is sunny",
-        location: "Hong Kong",
-        address: req.query.address
+
+    geocode(req.query.address, (error, {latitude, longitude, location} = {}) => {
+        if (error) {
+            return res.send({ error })
+        }
+
+        forecast(latitude, longitude, (error, forecastData) => {
+            if (error) {
+                return res.send({ error })
+            }
+
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
     })
+
 })
 
 app.get("/products", (req, res) => {
